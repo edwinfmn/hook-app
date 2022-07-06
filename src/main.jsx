32231c98c7b86781208d3c9476dbdddcc21840b9
edwin.martinez <EdwinSeb2014>
2022-07-06 13:25:27 -0500

    Config for testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 //import { CallbackHook } from './06-memos/CallbackHook'
 //import { Padre } from './07-tarea-memo/Padre'
 //import { MemoHook } from './06-memos/MemoHook'
@@ -8,13 +9,16 @@ import ReactDOM from 'react-dom/client'
 //import { Layout } from './05-useLayoutEffect/Layout'
 //import { Memorize } from './06-memos/Memorize'
 //import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp';
+//import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 import './index.css'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <TodoApp />
-  // </React.StrictMode>
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 )
